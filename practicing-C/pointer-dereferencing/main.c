3a83d387963115d049f6b17bdf703dc1f5e2cb71
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-09-24 21:18:59 +0000

    Main.c file

---
+++ practicing-C/pointer-dereferencing/main.c
@@ -0,0 +1,15 @@
+#include "header.h"
+/**
+*main - calls other functions
+*Return: Always 0
+*/
+int main(void)
+{
+  int n;
+  n = 402
+  printf("n = %d\n", n);
+  change_val(&n);
+  printf("n = %d\n", n);
+  return (0);
+}
+  
