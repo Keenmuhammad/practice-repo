6b2c66f6094172118f331c1ddce6b9698ad3efb3
Keenmuhammad <kibrahimbello@yahoo.com>
2022-09-24 22:57:57 -0700

    More practiced examples

--- practicing-C/pointer-dereferencing/header.h
+++ practicing-C/pointer-dereferencing/header.h
@@ -1,11 +1,1 @@
-#include "header.h"
-
-/**
- * change_val - changes the value
- * @n: integer to be changed to 72
- * Return: void
- */
-void change_val(int *n)
-{
-	*n = 72
-}
+void change_val(int *n);
--- practicing-C/pointer-dereferencing/main.c
+++ practicing-C/pointer-dereferencing/main.c
@@ -1,16 +1,16 @@
 #include "header.h"
 #include <stdio.h>
 /**
-*main - calls other functions
-*Return: Always 0
-*/
+ *main - calls other functions
+ *Return: Always 0
+ */
 int main(void)
 {
-  int n;
-  n = 402
-  printf("n = %d\n", n);
-  change_val(&n);
-  printf("n = %d\n", n);
-  return (0);
+	int n;
+
+	n = 402;
+		printf("n = %d\n", n);
+	change_val(&n);
+	printf("n = %d\n", n);
+	return (0);
 }
-  
